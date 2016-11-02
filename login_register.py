7a607fded23b9bf37c4e1f85564d013d7f63c11f
Karthik Raghunathan <kraghunathan@groupon.com>
2016-11-02 18:14:44 +0530

    added bar chart a la bostock

--- login_register.py
+++ login_register.py
@@ -31,13 +31,6 @@ def index():
         return render_template('login.html')
     return render_template('index.html')
 
-@app.route("/store")
-def store():
-    import fk
-    prods = fk.load_products('home_improvement_tools_')
-    c = fk.load_categories()
-    return render_template('store.html',categories=c,products=prods )
-
 @app.route("/logout")
 def logout():
     redirect_to_index = redirect("/")
@@ -45,18 +38,18 @@ def logout():
     response.set_cookie('token',value= '', expires=0)
     return response
 
-def login_and_redirect_to(path):
+def login_and_redirect_to( path, user):
     redirect_to_index = redirect(path)
     response = app.make_response(redirect_to_index )
     response.set_cookie('token',value='user:password')
-    return response
+    return render_template(path,email=user)
 
 @app.route("/login",methods=['GET', 'POST'])
 def do1():
     if request.method == 'POST':
-        print '37'
+        print 'log in request' ,request.form['email'] or 'None supplied' ,request.form['password'] or 'None supplied'
         user = load_user(request)
-        return login_and_redirect_to('/')
+        return login_and_redirect_to('/index.html', "aha@oho.com")
     print '32'
     return render_template('login.html')
 
--- store.py
+++ store.py
@@ -27,6 +27,15 @@ def rohn():
     print len(page1)
     return render_template( 'srohn.html', page = page1, numpages=pagenums, selectedpage=page_num)
 
+
+@app.route('/stocks')
+def stocks():
+    return render_template('stocks.html')
+
+@app.route('/data')
+def data():
+    return open('static/d3.csv').read()
+
 @app.route("/store")
 def store():
     import fk
@@ -34,23 +43,23 @@ def store():
     c = fk.load_categories()
     return render_template('store.html',categories=c,products=prods )
 
-@app.route("/map")
+@app.route("/map" ,methods=['GET', 'POST'])
 def map():
-    return render_template('map.html')
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
     if request.method == 'POST':
-        print 'logged in' ,request.form['email'] or 'None supplied' ,request.form['password'] or 'None supplied'
-        return render_template('store.html')
-    else:
-        return render_template('login.html')
-
-@app.route("/register")
-def register():
-    return render_template('register.html')
-
+        from geopy.geocoders import Nominatim
+        geolocator = Nominatim()
+        searchterm = request.form['searchterm']
+        print searchterm
+        location = geolocator.geocode( searchterm)
+        lat1 = location.raw['lat']
+        lon1 = location.raw['lon']
+        return render_template('map.html', coords=json.dumps([lat1,0,lon1,0]) )
+    lat1 = request.args.get('lat1');
+    lon1 = request.args.get('lon1');
+    lat2 = request.args.get('lat2');
+    lon2 = request.args.get('lon2');
+    print lat1,lat2,lon1,lon2
+    return render_template('map.html', coords=json.dumps([lat1,lat2,lon1,lon2]) )
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
