f34791fca6498e2ab25d113e9bf773caef093f2a
Karthik Raghunathan <kraghunathan@groupon.com>
2017-01-28 10:24:44 +0530

    3 col portfolio view with shopping cart that stores id to session

--- store.py
+++ store.py
@@ -1,3 +1,9 @@
+
+from flask import Flask,session, redirect, url_for, escape, request,Response,jsonify
+from flask import render_template
+
+import json
+
 from flask import Flask,session, redirect, url_for, escape, request,Response,jsonify
 from flask import render_template
 
@@ -9,30 +15,32 @@ app = Flask(__name__)
 def paper():
     return render_template('paper.html')
 
+@app.route("/")
+def col3p():
+    a = json.load( open( './json/toymatic_products.json'))
+    b = [a[x:x+3] for x in xrange(0, len(a), 3)]
+    return render_template('3colportfolio.html', prods = b, count=0)
 
-@app.route("/store")
-def store():
-    prods = json.load( open( './json/toymatic_products.json'))
-    cats = json.load( open( './json/toymatic_categories.json'))
-    return render_template('store.html',categories=cats,products=prods )
+@app.route("/add")
+def addToCart():
+    cart_id =  request.args.get('cart-id')
+    item_id = request.args.get('item-id')
 
-@app.route("/index")
-def index():
-    return render_template('index.html')
+    print cart_id, item_id
 
-@app.route("/homepage")
-def homepage():
-    return render_template('homepage.html')
+    c = session.get( 'count',0)
+    p = session.get( 'items-' + cart_id ,[])
+    p.append(item_id)
 
-@app.route("/homepagewo")
-def homepage1():
-    return render_template('homepage_wo_banner.html')
+    session[ 'count'] = c +1
+    session[ 'items-' + cart_id ] = p
 
-@app.route("/t")
-def testo():
-    prods = json.load( open( './json/toymatic_products.json'))
-    cats = json.load( open( './json/toymatic_categories.json'))
-    return render_template('test.html',categories=cats,products=prods )
+    print session
+
+    a = json.load( open( './json/toymatic_products.json'))
+    b = [a[x:x+3] for x in xrange(0, len(a), 3)]
+
+    return render_template('3colportfolio.html', prods = b, count = c )
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
