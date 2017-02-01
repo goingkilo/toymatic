626040173a2da861cbc26e1540777ada94675f1b
Karthik Raghunathan <kraghunathan@groupon.com>
2017-02-01 16:31:49 +0530

    login logout

--- shopping_cart.py
+++ shopping_cart.py
@@ -19,3 +19,25 @@ class Cart:
             if item_id in self.items:
                 self.items.remove(item_id)
         return item_id
+
+
+
+class User1:
+    def __init__(self, name, id, active=True):
+        self.name = name
+        self.id = id
+        self.active = active
+
+    def is_active(self):
+        # Here you should write whatever the code is
+        # that checks the database if your user is active
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+
+    def get(self, user_id):
+        return User( 'user', '1')
--- store.py
+++ store.py
@@ -1,19 +1,22 @@
 
-from flask import Flask,session, redirect, url_for, escape, request,Response,jsonify
+from flask import Flask, session, redirect, url_for, escape, request,Response,jsonify
 from flask import render_template
 #from flask_session import Session
 from redis_session import RedisSessionInterface
 import json,os,redis
-
 from shopping_cart import Cart
 
+from flask_login import LoginManager,login_required,login_user,UserMixin,logout_user
 
-# --- --- --- --- --- --- --- --- --- --- ---
+from flask import Flask,flash
+from flask_bcrypt import Bcrypt
+
+# --- --- --- --- --- --- APP STUFF  --- --- --- --- ---
 app = Flask(__name__)
 
+# --- --- --- --- --- --- REDIS STUFF  --- --- --- --- ---
 connection_string  = os.environ.get('REDIS_URL','redis://localhost:6379')
 r = redis.from_url(connection_string)
-print 'connectint to redis', r
 
 app.secret_key = os.urandom(24)
 
@@ -24,8 +27,15 @@ app.session_interface = RedisSessionInterface(redis=r)
 #sess.init_app(app)
 
 app.debug = True
-# --- --- --- --- --- --- --- --- --- --- ---
+# --- --- --- --- --- --- AUTH STUFF  --- --- --- --- ---
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
+bcrypt = Bcrypt(app)
+
+r.set('user_a@b.c', bcrypt.generate_password_hash('hunter2'))
+# --- --- --- --- --- --- --- --- --- --- ---
 
 @app.route("/paper")
 def paper():
@@ -43,12 +53,12 @@ def home():
 
         session[ 'cart'] = cart
 
-        return "OK"
+        return str(cart.get_item_count())
     else:
         p = products(3);
         cart = session.get( 'cart', Cart() )
         print cart.items
-        return render_template('home.html', prods = p, count=cart.get_item_count())
+        return render_template('home.html', prods = p, count=cart.get_item_count(), user='a@b.c')
 
 @app.route("/item")
 def item():
@@ -89,6 +99,87 @@ def products(batch_size=1):
         return a
     b = [a[x:x+batch_size] for x in xrange(0, len(a), batch_size)]
     return b
+
+## -------------- auth stuff ---------
+
+class User( UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    a = r.get('user_'+user_id)
+
+    if a:
+        return User(user_id)
+
+@app.route('/auth/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        userid = request.form['username']
+        password = request.form['password']
+
+        user = User(userid)
+        stored_hash = r.get('user_'+userid)
+        if not stored_hash:
+            return abort(401)
+
+        if not bcrypt.check_password_hash( stored_hash, password):
+            return abort(402)
+
+        login_user(user)
+
+        session.pop('_flashes', None)
+        flash('Logged in successfully.')
+
+        next = request.args.get('next') or '/'
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return abort(400)
+
+        return redirect(next or url_for('home'))
+
+@app.route("/user/home")
+@login_required
+def user_home():
+    user_id = request.args.get('user_id')
+    cart = session.get('cart')
+
+    return render_template('account.html', user='user user user', count=cart.get_item_count() or 0)
+
+@app.route('/auth/register', methods=[ 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email    = request.form['email']
+        password = request.form['password']
+        confirm  = request.form['confirm-password']
+
+        if password != confirm :
+            session.pop('_flashes', None)
+            session.pop('_flashes', None)
+            flash(' passwords do not match ','error' )
+            return render_template('login.html')
+
+        r.set( 'user_' + email,  bcrypt.generate_password_hash(password))
+
+        return redirect(url_for('login'))
+
+# https://github.com/maxcountryman/flask-login
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def is_safe_url(x):
+    return True
 
 if __name__ == "__main__":
     app.run()
